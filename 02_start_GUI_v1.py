fb0290d729b0ab2dea88855be51174f376b1a9b2
imoa67381 <52428585+imoa67381@users.noreply.github.com>
2020-03-09 12:13:10 +1300

    GUI iteration 2

--- 02_start_GUI_v1.py
+++ 02_start_GUI_v1.py
@@ -37,22 +37,22 @@ class Start:
         # Buttons go here...
         button_font = "Arial 12 bold"
         # Orange low stakes button...
-        self.lowstakes_button = Button(self.stakes_frame, text="Low ($5)",
+        self.low_stakes_button = Button(self.stakes_frame, text="Low ($5)",
                              command=lambda: self.to_game(1),
                              font=button_font, bg="#FF9933")
-        self.lowstakes_button.grid(row=0, column=0, pady=10)
+        self.low_stakes_button.grid(row=0, column=0, pady=10)
 
         # Yellow medium stakes button ...
-        self.lowstakes_button = Button(self.stakes_frame, text="Medium ($10)",
+        self.medium_stakes_button = Button(self.stakes_frame, text="Medium ($10)",
                         command=lambda: self.to_game(2),
                         font=button_font, bg="#FF9933")
-        self.lowstakes_button.grid(row=0, column=1, padx=5, pady=10)
+        self.medium_stakes_button.grid(row=0, column=1, padx=5, pady=10)
 
         # Green high stakes button...
-        self.lowstakes_button = Button(self.stakes_frame, text="High ($15)",
+        self.high_stakes_button = Button(self.stakes_frame, text="High ($15)",
                         command=lambda: self.to_game(3),
                         font=button_font, bg="#99FF33")
-        self.lowstakes_button.grid(row=0, column=2, pady=10)
+        self.high_stakes_button.grid(row=0, column=2, pady=10)
 
         # Help Button
         self.help_button = Button(self.start_frame, text="How to Play",
